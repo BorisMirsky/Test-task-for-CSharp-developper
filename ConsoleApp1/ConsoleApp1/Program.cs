fda4ec2e5ff4ca01595392b086a8a7469062f128
Boris Mirsky <boris.y.mirsky@gmail.com>
2026-03-11 13:50:40 +0300

    ca ira

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Diagnostics.Metrics;
 
 namespace ConsoleApp1
 {
@@ -7,10 +8,40 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            foreach (string arg in args)
+            string[] splitted = args;
+            int h = 0; 
+            try
             {
-                Console.WriteLine(arg);
+                h = int.Parse(splitted[0]);
             }
+            catch (FormatException)
+            {
+                Console.WriteLine("The string is not in a valid format.");
+            }
+            int w = 0; 
+            try
+            {
+                w = int.Parse(splitted[1]);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("The string is not in a valid format.");
+            }
+
+            string[] alphabet = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z".Split(",")[0..w];
+            Dictionary<string, object> dict_result = new Dictionary<string, object>();
+            int counter = 0;     
+            for (int i = 1; i < (h + 1); i++)
+            {
+                for (int j = 0; j < w; j++)
+                {
+                    string k = alphabet[j] + i.ToString();
+                    dict_result[k] = splitted[2..w][counter];
+                    counter += 1;
+                }
+            }
+            Debug.WriteLine(dict_result);
         }
     }
 }
+
