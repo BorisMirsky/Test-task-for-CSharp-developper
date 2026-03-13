aefda62f0f7643f731e137e813bdc0aa59e5f8e9
Boris Mirsky <boris.y.mirsky@gmail.com>
2026-03-13 13:13:10 +0300

    create db, create table. insert data

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,6 +1,16 @@
-﻿using System;
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System;
+using System.Collections.Generic;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Data;
 using System.Diagnostics;
 using System.Diagnostics.Metrics;
+using System.Xml.Linq;
+//using System.Data.SQLite;
+
+
 
 
 namespace ConsoleApp1
@@ -8,14 +18,24 @@ namespace ConsoleApp1
     class Program
     {
 
+        //static readonly string[] args = ["3", "4", "12", "=C2", "3", "`Sample", "=A1+B1C1/5", "=A2B1", "=B3-C3", "`Spread", "`Test", "=4-3", "5", "`Sheet"];
+
         static void Main(string[] args)
         {
             args = ["3", "4", "12", "=C2", "3", "`Sample", "=A1+B1C1/5", "=A2B1", "=B3-C3", "`Spread", "`Test", "=4-3", "5", "`Sheet"];
-            MyMethod(args); 
+            MyMethod(args);
+            CreateDB(dict_result, args);
         }
 
+        
         static Dictionary<string, object> dict_result = new Dictionary<string, object>();
 
+        readonly static string dbName = "MyDatabase.db";
+
+        readonly static string connectionString = $"Data Source=C:\\Users\\Alexander\\source\\.Net\\Test-task-for-CSharp-developper\\ConsoleApp1\\ConsoleApp1\\Database\\{dbName}";
+
+        //readonly static string alphabet = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"; //.Split(",")[0..w];
+        
         static void MyMethod(string[] args)
         {
             int h = 0;
@@ -25,7 +45,7 @@ namespace ConsoleApp1
             }
             catch (FormatException)
             {
-                Console.WriteLine("The string is not in a valid format.");
+                Debug.WriteLine("The string is not in a valid format.");
             }
 
             int w = 0;
@@ -35,17 +55,20 @@ namespace ConsoleApp1
             }
             catch (FormatException)
             {
-                Console.WriteLine("The string is not in a valid format.");
+                Debug.WriteLine("The string is not in a valid format.");
             }
 
+            //string[] alphabetSlice = alphabet.Split(",")[0..w];
             string[] alphabet = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z".Split(",")[0..w];
+            //string[] alphabetSlice = alphabet.Split(",")[0..w];
             int counter = 0;        // сквозной счетчик
+
             for (int i = 1; i < (h + 1); i++)
-            {   
+            {
                 for (int j = 0; j < w; j++)
                 {
                     string k = alphabet[j] + i.ToString();   // key for dictionary
-                    
+
                     // '`' string 
                     if (args[2..][counter][0] == '`')
                     {
@@ -57,7 +80,7 @@ namespace ConsoleApp1
                     {
                         dict_result[k] = (string)args[2..][counter];
                     }
-                    
+
                     // int
                     else
                     {
@@ -71,7 +94,7 @@ namespace ConsoleApp1
                             Console.WriteLine($"Unable to parse");
                         }
                     }
-                        counter += 1;
+                    counter += 1;
                 }
             }
 
@@ -95,35 +118,45 @@ namespace ConsoleApp1
         // Рекурсивное вычисление значения ячейки
         static int EvaluateCell(string cell)
         {
-            object value = dict_result[cell];
-            if (value is int)
-                return (int)value;
-            if (value is string s)
+            //int result = 0;
+            try
             {
-                if (s.StartsWith("="))
+                object value = dict_result[cell];
+                if (value is int)
+                    return (int)value;
+                if (value is string s)
                 {
-                    string expr = s.Substring(1);
-                    var (operands, ops) = ParseExpression(expr);
-                    int result = GetOperandValue(operands[0]);
-                    for (int i = 0; i < ops.Count; i++)
+                    if (s.StartsWith("="))
                     {
-                        int next = GetOperandValue(operands[i + 1]);
-                        switch (ops[i])
+                        string expr = s.Substring(1);
+                        var (operands, ops) = ParseExpression(expr);
+                        int result = GetOperandValue(operands[0]);
+                        for (int i = 0; i < ops.Count; i++)
                         {
-                            case '+': result += next; break;
-                            case '-': result -= next; break;
-                            case '*': result *= next; break;
-                            case '/': result /= next; break;
+                            int next = GetOperandValue(operands[i + 1]);
+                            switch (ops[i])
+                            {
+                                case '+': result += next; break;
+                                case '-': result -= next; break;
+                                case '*': result *= next; break;
+                                case '/': result /= next; break;
+                            }
                         }
+                        dict_result[cell] = result; // сохраняем результат
+                        return result;
                     }
-                    dict_result[cell] = result; // сохраняем результат
-                    return result;
-                }
-                else
-                {
-                    throw new InvalidOperationException($"Cell {cell} contains a string, cannot be used in expression");
+                    else
+                    {
+                        throw new InvalidOperationException($"Cell {cell} contains a string, cannot be used in expression");
+                    }
                 }
             }
+            catch(KeyNotFoundException e)
+            {
+                dict_result[cell] = "#Error";
+                Debug.WriteLine(e);
+            }
+            //return result;
             throw new InvalidOperationException($"Cell {cell} has invalid type");
         }
 
@@ -186,6 +219,65 @@ namespace ConsoleApp1
             }
             return (operands, ops);
         }
+      
+        static void CreateDB(Dictionary<string, object> d, string[] a)
+        {
+            try
+            {
+                using (var connection = new SqliteConnection(connectionString))
+                {
+                    connection.Open();
+                    Debug.WriteLine($"{dbName} created or opened successfully.");
+                    CreateTable(connection, a);
+                    InsertData(connection, dict_result);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+
+        static void CreateTable(SqliteConnection connection, string[] a)
+        {
+            string[] alphabet = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z".Split(",");
+            string createTableQuery = @"
+                    CREATE TABLE IF NOT EXISTS Cells (
+                        Id INTEGER PRIMARY KEY AUTOINCREMENT,";
+            for (int i = 0; i < int.Parse(a[1]); i++)
+            {
+                string addColumn = $"{alphabet[i]} TEXT NOT NULL,";
+                createTableQuery += addColumn;
+            }
+            createTableQuery = createTableQuery.TrimEnd(',') + ");";
+
+            using (var command = new SqliteCommand(createTableQuery, connection))
+            {
+                command.ExecuteNonQuery();
+            }
+        }
+
+        static void InsertData(SqliteConnection connection, Dictionary<string, object> d)   //, string[] cols)
+        {
+            //"INSERT INTO Users (Name, Age) VALUES ('Alice', 32), ('Bob', 28)";
+            string sqlExpression = "INSERT INTO Cells (ColumnsNames) VALUES (CellsValues)";
+            foreach (KeyValuePair<string, object> entry in dict_result)
+            {
+                //Console.WriteLine($"Key = {entry.Key}, Value = {entry.Value}");
+
+            }
+
+
+            using (connection)
+            {
+                connection.Open();
+
+                SqliteCommand command = new SqliteCommand(sqlExpression, connection);
+
+                int number = command.ExecuteNonQuery();
+
+                //Console.WriteLine($"В таблицу Users добавлено объектов: {number}");
+            }
+        }
     }
 }
-
